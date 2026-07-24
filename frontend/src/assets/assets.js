93c22f7a7d84e7d50ef8b12c42265a10c4ce07a5
Samuel Ogwuche <samogwuche98@gmail.com>
2026-07-24 22:49:49 +0100

    setup frontend architecture

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,35 @@
+import cross_icon from "./cross_icon.png";
+import close_icon from "./close_icon.svg";
+import facebook_icon from "./facebook_icon.png";
+import linkedin_icon from "./linkedin_icon.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import search_icon from "./search_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import menu_icon from "./menu_icon.svg";
+import profile_icon from "./profile_icon.png";
+import logo_dark from "./logo-dark.png";
+import logo_light from "./logo-light.png";
+import users_three from "./users-three.svg";
+import users_two from "./users-two.svg";
+import location_pin from "./location-pin.svg";
+import heart_icon from "./heart_icon.svg";
+
+export const assets = {
+  cross_icon,
+  close_icon,
+  facebook_icon,
+  linkedin_icon,
+  search_icon,
+  app_store,
+  play_store,
+  twitter_icon,
+  menu_icon,
+  profile_icon,
+  logo_dark,
+  logo_light,
+  users_three,
+  users_two,
+  location_pin,
+  heart_icon,
+};
