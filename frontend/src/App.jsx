0fd61cc06d13d55561e2d8d4a603bbd2d488db40
Samuel Ogwuche <samogwuche98@gmail.com>
2026-08-03 01:00:52 +0100

    implement react-router-dom function for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import FAQPage from "./pages/FAQs/FAQPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
